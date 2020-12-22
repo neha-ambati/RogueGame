b66b80a6e5d0a9fefee636767d0096b423da9b99
neha-ambati <76458240+neha-ambati@users.noreply.github.com>
2020-12-22 15:27:18 -0500

    Add files via upload

---
+++ src/main/java/rogue/NoSuchItemException.java
@@ -0,0 +1,10 @@
+package rogue;
+
+public class NoSuchItemException extends Exception {
+
+    /**
+     * Default constructor.
+     */
+    public NoSuchItemException() {
+    }
+}
---
+++ src/main/java/rogue/NotEnoughDoorsException.java
@@ -0,0 +1,10 @@
+package rogue;
+
+public class NotEnoughDoorsException extends Exception {
+
+    /**
+     * Default constructor.
+     */
+    public NotEnoughDoorsException() {
+    }
+}
